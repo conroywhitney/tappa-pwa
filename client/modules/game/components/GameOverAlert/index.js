b807221210714acd4f3585448b18d01dac71c9a7
Conroy Whitney <conroy.whitney@gmail.com>
2017-03-06 22:39:14 -0900

    Change Dialog font size with react-toolbox theme

--- client/modules/game/components/GameOverAlert/index.js
+++ client/modules/game/components/GameOverAlert/index.js
@@ -2,8 +2,11 @@
 
 import React from 'react'
 import RTDialog from 'react-toolbox/lib/dialog'
+import { themr } from 'react-css-themr'
 
 import { STATUS } from '../../../../constants'
+
+import dialogTheme from './theme.scss'
 
 const WonDialog = props => (
   <RTDialog {...props} title='You win!'>
@@ -22,7 +25,7 @@ const dialogs = {
   [STATUS.won]: WonDialog
 }
 
-export default function GameOverAlert({ status, onPress }) {
+function GameOverAlert({ status, theme, onPress }) {
   const closeDialog = onPress
   const actions = [{ label: 'Play Again', onClick: closeDialog }]
   const Dialog = dialogs[status]
@@ -34,8 +37,12 @@ export default function GameOverAlert({ status, onPress }) {
       <Dialog
           actions={actions}
           active
+          className={theme.dialog}
+          theme={theme}
           onEscKeyDown={closeDialog}
           onOverlayClick={closeDialog} />
     </div>
   )
 }
+
+export default themr('', dialogTheme)(GameOverAlert)
