be12bf6dbf0bd232da76ad19709111c84a1164a6
Conroy Whitney <conroy.whitney@gmail.com>
2017-03-06 16:29:44 -0900

    GameOverAlert uses `react-toolbox` `Dialog` for won/lost

--- client/modules/game/components/GameOverAlert/index.js
+++ client/modules/game/components/GameOverAlert/index.js
@@ -1,11 +1,41 @@
 // @flow
 
 import React from 'react'
+import RTDialog from 'react-toolbox/lib/dialog'
 
-export default function GameOverAlert() {
+import { STATUS } from '../../../../constants'
+
+const WonDialog = props => (
+  <RTDialog {...props} title='You win!'>
+    Congratulations! You have won!
+  </RTDialog>
+)
+
+const LostDialog = props => (
+  <RTDialog {...props} title='You lost :('>
+    Aww, so close! Try again!
+  </RTDialog>
+)
+
+const dialogs = {
+  [STATUS.lost]: LostDialog,
+  [STATUS.won]: WonDialog
+}
+
+export default function GameOverAlert({ status, onPress }) {
+  const closeDialog = onPress
+  const actions = [{ label: 'Play Again', onClick: closeDialog }]
+  const Dialog = dialogs[status]
+
+  if (!Dialog) return null
+
   return (
     <div>
-      <h4>GameOverAlert</h4>
+      <Dialog
+          actions={actions}
+          active
+          onEscKeyDown={closeDialog}
+          onOverlayClick={closeDialog} />
     </div>
   )
 }
--- client/modules/game/components/SinglePlayer/index.js
+++ client/modules/game/components/SinglePlayer/index.js
@@ -8,7 +8,6 @@ import GameOverAlert from '../GameOverAlert'
 export default function SinglePlayer({ board, status, handleTap, resetGame }) {
   return (
     <div>
-      <h4>Single Player</h4>
       <GameOverAlert status={status} onPress={resetGame} />
       <Board board={board} onClick={handleTap} />
     </div>
